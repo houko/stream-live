689ff50eae370a4b2c25ea568d7ad05ab54b9159
Evan <suzukaze.hazuki2020@gmail.com>
2023-01-20 22:07:39 +0900

    stream

---
+++ .vimrc
@@ -0,0 +1,4 @@
+syntax on
+set fileencodings=utf-8,gb2312,gb18030,gbk,ucs-bom,cp936,latin1
+set enc=utf8
+set fencs=utf8,gbk,gb2312,gb18030
---
+++ init.sh
@@ -0,0 +1,8 @@
+sudo yum update
+sudo yum upgrade
+sudo yum install -y screen
+sudo yum install -y vim
+
+sudo cp .screenrc ~/
+sudo cp .vimrc ~/
+sudo mkdir ~/video
